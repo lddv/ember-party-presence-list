5c091349405bf99921eba2590b5f66dc3c354f3d
Leonardo Dias de Vasconcelos <leodias.vasconcelos@yahoo.com.br>
2014-10-06 15:13:36 -0300

    Toggle property inside model, no persistence

--- js/app.js
+++ js/app.js
@@ -1,11 +1,54 @@
-App = Ember.Application.create();
+var App = Ember.Application.create();
 
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+// ROUTER
 App.Router.map(function() {
-  // put your routes here
+  this.resource('guests', { path: '/' });
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.GuestsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('guest');
   }
 });
+
+// CONTROLLER
+App.GuestsController = Ember.ArrayController.extend({
+  sortProperties: ['name'],
+  itemController: 'guest'
+});
+
+App.GuestController = Ember.ObjectController.extend({
+  actions: {
+    toggleAttendance: function(value){
+      var guest = this.get('model');
+      guest.set('guestHasAttended', value);
+    }
+  }
+});
+
+// MODEL
+App.Guest = DS.Model.extend({
+  name: DS.attr('string'),
+  guestHasAttended: DS.attr('boolean')
+});
+
+App.Guest.FIXTURES = [
+{
+  id: 1,
+  name: 'Leonardo',
+  guestHasAttended: false
+}, {
+  id: 2,
+  name: 'Denise',
+  guestHasAttended: false
+}, {
+  id: 3,
+  name: 'Antonio',
+  guestHasAttended: false
+}, {
+  id: 4,
+  name: 'Leticia',
+  guestHasAttended: false
+}];
